f83f67d5b08f402203792ebd4068e60079b6e86d
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2021-08-21 16:10:01 +0200

    Create basic_install.sh

---
+++ basic_install/basic_install.sh
@@ -0,0 +1,29 @@
+#----- Basic install Script
+
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install fail2ban
+
+ sudo apt install unattended-upgrades apt-listchanges bsd-mailx
+
+ sudo dpkg-reconfigure -plow unattended-upgrades
+ 
+
+ #sudo vi /etc/apt/apt.conf.d/50unattended-upgrades
+
+
+ #Unattended-Upgrade::Mail "xyz@abc.com";
+
+
+ #Unattended-Upgrade::Automatic-Reboot "true";
+
+
+ #email_address=xyz@abc.com
+
+
+sudo unattended-upgrades --dry-run
+
+sudo ufw allow ssh
+
+sudo ufw enable
