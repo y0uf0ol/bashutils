2980729b54262d788e124d84d88175baff51c42e
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2021-08-21 17:16:50 +0200

    Update basic_install.sh

--- basic_install/basic_install.sh
+++ basic_install/basic_install.sh
@@ -13,7 +13,8 @@ sudo sed -i 's/Unattended-Upgrade::Mail "";/Unattended-Upgrade::Mail "$Email";/g
 
 sudo sed -i 's/email_address=root/email_address=$Email/g' /etc/apt/listchanges.conf
 
-
+echo "you can find the logs here /var/log/unattended-upgrades/unattended-upgrades.log"
+echo " or you can configure a SMTP Server to get mails"
 
 
 sudo ufw allow ssh
