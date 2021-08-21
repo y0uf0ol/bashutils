60f4c564901836e098b7d6ed03df22abbd31ba75
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2021-08-21 16:55:21 +0200

    Update basic_install.sh

--- basic_install/basic_install.sh
+++ basic_install/basic_install.sh
@@ -1,20 +1,20 @@
 #----- Basic install Script
 
-
-
+echo "Enter your Mail Adress"
+read -p "" Email
 
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install fail2ban unattended-upgrades apt-listchanges -y
 
 sudo dpkg-reconfigure -plow unattended-upgrades --default-priority
- 
-sudo sed -i 's/Unattended-Upgrade::Mail "";/Unattended-Upgrade::Mail "YOURMAIL";/g' /etc/apt/apt.conf.d/50unattended-upgrades
 
-sudo sed -i 's/email_address=root/email_address=YOURMAIL/g' /etc/apt/listchanges.conf
+sudo sed -i 's/Unattended-Upgrade::Mail "";/Unattended-Upgrade::Mail "$Email";/g' /etc/apt/apt.conf.d/50unattended-upgrades
+
+sudo sed -i 's/email_address=root/email_address=$Email/g' /etc/apt/listchanges.conf
 
 
- 
+
 
 sudo ufw allow ssh
 
